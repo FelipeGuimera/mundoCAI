5af3b9b148bc999607f9782d379c1db9d637a19a
Felipe <91702841+FelipeGuimera@users.noreply.github.com>
2023-07-24 12:51:02 -0300

    add fragments

---
+++ app/src/main/java/com/example/mundocai/ui/HomeFragment.kt
@@ -0,0 +1,16 @@
+package com.example.mundocai.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.View
+import com.example.mundocai.R
+
+
+class HomeFragment : Fragment(R.layout.fragment_home) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
+
+
+
