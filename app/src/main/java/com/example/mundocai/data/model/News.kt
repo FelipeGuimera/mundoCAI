0be1e44c341ffdacce66cca5a17bf2acbe945404
Felipe <91702841+FelipeGuimera@users.noreply.github.com>
2023-07-25 00:01:55 -0300

    bwh

---
+++ app/src/main/java/com/example/mundocai/data/model/News.kt
@@ -0,0 +1,8 @@
+package com.example.mundocai.data.model
+
+data class News(
+    val image_news: String = "",
+    val title_news: String = "",
+    val description_news: String = "",
+    val news_date: String = ""
+)
---
+++ app/src/main/java/com/example/mundocai/ui/home/adapter/NewsAdapter.kt
@@ -0,0 +1,22 @@
+package com.example.mundocai.ui.home.adapter
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.mundocai.core.BaseViewHolder
+import com.example.mundocai.data.model.News
+
+class NewsAdapter (private val newsList: List<News>): RecyclerView.Adapter<BaseViewHolder<*>>(){
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BaseViewHolder<*> {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: BaseViewHolder<*>, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+}
