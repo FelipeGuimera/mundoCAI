fb73234b28cfdce1a51bf0a062e9d878d344a90b
Felipe <91702841+FelipeGuimera@users.noreply.github.com>
2023-09-26 12:12:08 -0300

    login-register screen

--- app/src/main/java/com/example/mundocai/MainActivity.kt
+++ app/src/main/java/com/example/mundocai/MainActivity.kt
@@ -1,32 +1,55 @@
 package com.example.mundocai
 
+import android.app.Notification.Action
 import android.os.Bundle
 import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
+import com.example.mundocai.core.hide
+import com.example.mundocai.core.show
 import com.example.mundocai.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        val navController = navHostFragment.navController
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
         binding.bottomNavigationView.setupWithNavController(navController)
 
 
-        supportActionBar?.displayOptions = ActionBar.DISPLAY_SHOW_CUSTOM
-        supportActionBar?.setCustomView(R.layout.toolbar)
-
-
-
+        observeDestinationChange()
 
     }
 
+    private fun observeDestinationChange() {
+        navController.addOnDestinationChangedListener { controller, destination, arguments ->
+            when (destination.id) {
+                R.id.loginFragment -> {
+                    binding.bottomNavigationView.hide()
+                    getSupportActionBar()?.hide()
+
+
+                }
+
+                else -> {
+                    binding.bottomNavigationView.show()
+                    supportActionBar?.displayOptions = ActionBar.DISPLAY_SHOW_CUSTOM
+                    supportActionBar?.setCustomView(R.layout.toolbar)
+                }
+            }
+        }
+    }
+
+
 }
+
--- app/src/main/java/com/example/mundocai/ui/auth/LoginFragment.kt
+++ app/src/main/java/com/example/mundocai/ui/auth/LoginFragment.kt
@@ -5,9 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
 import com.example.mundocai.R
+import com.example.mundocai.core.Resource
+import com.example.mundocai.data.remote.auth.LoginDataSource
 import com.example.mundocai.databinding.FragmentLoginBinding
+import com.example.mundocai.domain.auth.LoginRepoImpl
+import com.example.mundocai.presentation.auth.LoginScreenViewModel
+import com.example.mundocai.presentation.auth.LoginScreenViewModelFactory
 import com.google.firebase.auth.FirebaseAuth
 
 class LoginFragment : Fragment(R.layout.fragment_login) {
@@ -15,10 +23,17 @@ class LoginFragment : Fragment(R.layout.fragment_login) {
 
     private lateinit var binding: FragmentLoginBinding
     private val firebaseAuth by lazy { FirebaseAuth.getInstance() }
+    private val viewModel by viewModels<LoginScreenViewModel> { LoginScreenViewModelFactory(
+        LoginRepoImpl(
+        LoginDataSource()
+    )
+    ) }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentLoginBinding.bind(view)
+        isUserLoggedIn()
+        doLogin()
     }
 
 
@@ -32,6 +47,8 @@ class LoginFragment : Fragment(R.layout.fragment_login) {
         binding.btnSignin.setOnClickListener {
             val email = binding.editTextEmail.text.toString().trim()
             val password = binding.editTextPassword.text.toString().trim()
+            validateCredentials(email, password)
+            signIn(email,password)
         }
     }
 
@@ -45,6 +62,35 @@ class LoginFragment : Fragment(R.layout.fragment_login) {
             binding.editTextPassword.error = "Por favor, complete este campo"
             return
         }
+    }
+
+    private fun signIn(email: String, password: String){
+        viewModel.signIn(email,password).observe(viewLifecycleOwner, Observer { result ->
+            when (result) {
+                is Resource.Loading -> {
+                    binding.progressBar.visibility = View.VISIBLE
+                    binding.btnSignin.isEnabled = false
+                }
+                is Resource.Success -> {
+                    binding.progressBar.visibility = View.GONE
+                    Toast.makeText(
+                        requireContext(),
+                        "Welcome ${result.data?.email}",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    findNavController().navigate(R.id.action_loginFragment_to_homeFragment)
+                }
+                is Resource.Failure -> {
+                    binding.btnSignin.isEnabled = true
+                    binding.progressBar.visibility = View.GONE
+                    Toast.makeText(
+                        requireContext(),
+                        "Error: ${result.exception}",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+        })
     }
 
 }
